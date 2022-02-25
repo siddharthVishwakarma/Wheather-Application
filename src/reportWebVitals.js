9f81ce7f96ccba112613a1385d8e7068a90cfba9
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-02-26 02:00:50 +0530

    v1

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -11,3 +11,7 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
+
+// 452fd341c764d795438a4bd03a6f3f4e
+
+// api.openweathermap.org/data/2.5/find?q=London&appid=452fd341c764d795438a4bd03a6f3f4e
